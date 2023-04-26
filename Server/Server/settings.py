0e5f7bac873e2816c525ccd64bceb9c1981c84fb
user <user@ubuntu.localdomain>
2023-04-26 18:57:28 +0800

    update

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'NAME': 'CTFz',
         'USER': 'CTFz',
         'PASSWORD': 'CTFz',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
